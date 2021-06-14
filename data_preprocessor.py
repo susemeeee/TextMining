5c98f8fbcaf8128fac5a78c04d0f8b7c020d8695
susemeeee <hffg123@naver.com>
2021-06-14 21:00:54 +0900

    Add extract test data

--- data_preprocessor.py
+++ data_preprocessor.py
@@ -1,5 +1,8 @@
 import codecs
 import csv
+import os
+import shutil
+import random
 
 
 def classificate_file(filename):
@@ -11,10 +14,10 @@ def classificate_file(filename):
     for cell in reader:
         try:
             if int(cell[3]) > 3:
-                f = open(make_filename(positive_count, True), "w")
+                f = open(make_filename(positive_count, True, False), "w")
                 positive_count += 1
             else:
-                f = open(make_filename(negative_count, False), "w")
+                f = open(make_filename(negative_count, False, False), "w")
                 negative_count += 1
 
             f.write(cell[4])
@@ -23,8 +26,13 @@ def classificate_file(filename):
             continue
 
 
-def make_filename(count, positive):
-    new_filename = "../data/train/"
+def make_filename(count, positive, extract):
+    new_filename = "../data/"
+
+    if extract:
+        new_filename += "test/"
+    else:
+        new_filename += "train/"
 
     if positive:
         new_filename += "pos/review_pos_"
@@ -35,3 +43,33 @@ def make_filename(count, positive):
     new_filename += ".txt"
 
     return new_filename
+
+
+def extract_test_data(data_count):
+    positive_path = "../data/train/pos"
+    negative_path = "../data/train/neg"
+    pos_file_list = os.listdir(positive_path)
+    neg_file_list = os.listdir(negative_path)
+
+    pos_numbers = set()
+    neg_numbers = set()
+    pos_file_count = len(pos_file_list)
+    neg_file_count = len(neg_file_list)
+
+    for i in range(0, data_count):
+        random_index = random.randrange(1, pos_file_count + 1)
+        while random_index in pos_numbers:
+            random_index = random.randrange(1, pos_file_count + 1)
+
+        pos_numbers.add(random_index)
+
+        random_index = random.randrange(1, neg_file_count + 1)
+        while random_index in neg_numbers:
+            random_index = random.randrange(1, neg_file_count + 1)
+
+        neg_numbers.add(random_index)
+
+    for n in pos_numbers:
+        shutil.move(make_filename(n, True, False), make_filename(n, True, True))
+    for n in neg_numbers:
+        shutil.move(make_filename(n, False, False), make_filename(n, False, True))
--- main.py
+++ main.py
@@ -2,3 +2,4 @@ import data_preprocessor
 
 if __name__ == '__main__':
     data_preprocessor.classificate_file('review.csv')
+    data_preprocessor.extract_test_data(10000)
